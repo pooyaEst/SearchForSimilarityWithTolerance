ec8c45d1ae1ce5d601ab74089e67146461ee8171
pooya Games <426713+pooyaEst@users.noreply.github.com>
2024-12-31 21:25:29 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your-package-name",
+    name="    SearchForSimilarityWithTolerance",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
